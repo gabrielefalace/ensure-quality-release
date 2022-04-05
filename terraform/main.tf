fb4ee8d924fb8d1b83362a902b6da77536999233
Gabriele Falace <gabrielefalace@gmail.com>
2022-04-05 10:49:26 +0200

    initial commit

---
+++ terraform/main.tf
@@ -0,0 +1,60 @@
+provider "azurerm" {
+  tenant_id       = "${var.tenant_id}"
+  subscription_id = "${var.subscription_id}"
+  client_id       = "${var.client_id}"
+  client_secret   = "${var.client_secret}"
+  features {}
+}
+
+
+
+#module "resource_group" {
+#  source               = "./modules/resource_group"
+#  resource_group       = "${var.resource_group}"
+#  location             = "${var.location}"
+#}
+
+module "network" {
+  source               = "./modules/network"
+  address_space        = "${var.address_space}"
+  location             = "${var.location}"
+  virtual_network_name = "${var.virtual_network_name}"
+  application_type     = "${var.application_type}"
+  resource_type        = "NET"
+  resource_group       = "${var.resource_group}"
+  address_prefix_test  = "${var.address_prefix_test}"
+}
+
+module "nsg-test" {
+  source           = "./modules/networksecuritygroup"
+  location         = "${var.location}"
+  application_type = "${var.application_type}"
+  resource_type    = "NSG"
+  resource_group   = "${var.resource_group}"
+  subnet_id        = "${module.network.subnet_id_test}"
+  address_prefix_test = "${var.address_prefix_test}"
+}
+
+module "appservice" {
+  source           = "./modules/appservice"
+  location         = "${var.location}"
+  application_type = "${var.application_type}"
+  resource_type    = "AppService"
+  resource_group   = "${var.resource_group}"
+}
+module "publicip" {
+  source           = "./modules/publicip"
+  location         = "${var.location}"
+  application_type = "${var.application_type}"
+  resource_type    = "publicip"
+  resource_group   = "${var.resource_group}"
+}
+
+terraform {
+  backend "azurerm" {
+    resource_group_name =  "ens-qua-rel"
+    storage_account_name = "tfstate3ensquarel"
+    container_name       = "tfstate"
+    key           = "terraform.tfstate"
+  }
+}
---
+++ terraform/terraform.tfvars
@@ -0,0 +1,17 @@
+# Azure subscription vars
+subscription_id = "efa42e01-27ef-4660-b3e7-2f62a24bd429"
+
+#leaving these credentials blank should make use of the currently logged in user.
+client_id = ""
+client_secret = ""
+tenant_id = ""
+
+# Resource Group/Location
+location = "East US 2"
+resource_group = "ens-qua-rel"
+application_type = "EnsQuaRel-appType"
+
+# Network
+virtual_network_name = "EnsQuaRel-VNet"
+address_space = ["10.5.0.0/16"]
+address_prefix_test = ["10.5.1.0/24"]
