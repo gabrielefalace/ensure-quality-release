6ddef0ee463d712c922e7583c984d9ef611e4590
Gabriele Falace <gabrielefalace@gmail.com>
2022-04-14 11:53:13 +0200

    fix: missing names

--- terraform/modules/vm/vm.tf
+++ terraform/modules/vm/vm.tf
@@ -1,5 +1,5 @@
-resource "azurerm_network_interface" "" {
-  name                = ""
+resource "azurerm_network_interface" "test" {
+  name                = "Ens-Qua-Rel-NIC"
   location            = "${var.location}"
   resource_group_name = "${var.resource_group}"
 
@@ -11,7 +11,7 @@ resource "azurerm_network_interface" "" {
   }
 }
 
-resource "azurerm_linux_virtual_machine" "" {
+resource "azurerm_linux_virtual_machine" "test" {
   name                = "Ens-Qua-Rel-VM"
   location            = "${var.location}"
   resource_group_name = "${var.resource_group}"
